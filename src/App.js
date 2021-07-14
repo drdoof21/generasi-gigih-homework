b14921167deb09ea853938da21e3847d63109e2c
drdoof21 <ilhamrdh21@gmail.com>
2021-07-14 16:14:17 +0700

    homework pertama coba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import data from "./datadummy";
+import react from 'react';
+
+class App extends react.Component{
+  render() {
+    return (
+      <div>
+        <h1>Create Playlist</h1>
+  
+          <label>Title</label><br/>
+          <input type="text" /><br/>
+          <label>Description</label><br/>
+          <input type="text"/><br/>
+          <button id="button">Submit</button>
+          <div style={{border: "black solid 2px", margin: "20px 20px",padding: "10px"}}>
+          <img src={data.album.images[0].url}/><br/>
+          <label id="lTitle">{data.name}</label><br/>
+          <label id="lArtist">{data.artists[0].name}</label><br/>
+          <label id="lAlbum"></label>{data.album.name}<br/>
+          <button id="pencetan" onClick={this.isiSemua}>Button</button>
+          </div>
+      </div>
+    );
+    }
+    // isiSemua() {
+    //   const trackTitleDOM = document.getElementById('lTitle')
+    //   const coba = 'hehe';
+    //   trackTitleDOM.innerHTML = {coba};
+    // } 
+  
 }
+// react.findDOMNode(this.refs.) click(function(){
+
+
 
 export default App;
---
+++ src/pages/home/index.js
@@ -0,0 +1,18 @@
+//import reportWebVitals from './reportWebVitals';
+
+function Indexs() {
+  return(
+    <div>
+    <input type="search"></input>
+    <input type="button" value="pencet"></input><br/>
+    <img src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif" alt="hehe"></img>
+    </div>
+  );
+}
+export default Indexs;
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
